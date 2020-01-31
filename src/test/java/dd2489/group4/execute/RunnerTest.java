a7b4fb2d110f34d5043dccca5681b47ed639a226
Lizzie Queen <louiseze@kth.se>
2020-01-31 13:52:53 +0100

    refactor: remove old test package. Fix #11

--- src/test/java/dd2489/group4/execute/RunnerTest.java
+++
@@ -1,7 +0,0 @@
-package dd2489.group4.execute;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RunnerTest {
-
-}
--- src/test/java/dd2489/group4/net/ServerTest.java
+++
@@ -1,7 +0,0 @@
-package dd2489.group4.net;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ServerTest {
-
-}
