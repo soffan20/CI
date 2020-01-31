5278c8c5f15a7ee3a748a2dcfe37a2ad2c07db6a
Mikael Carlsson <mikkar2@kth.se>
2020-01-31 16:02:55 +0100

    net: defined structure for webhook push data fix #5
    added jackson library for json parsing

--- src/main/java/dd2480/group4/net/RequestHandler.java
+++ src/main/java/dd2480/group4/net/RequestHandler.java
@@ -30,7 +30,7 @@ public class RequestHandler extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         var bytes = request.getInputStream().readAllBytes();
         var json = new String(bytes, StandardCharsets.UTF_8);
-        var buildRequest = new BuildRequest(json);
+        var buildRequest =  BuildRequest.fromJson(json);
         executor.runBuild(buildRequest);
         baseRequest.setHandled(true);
         response.getWriter().println(json);
---
+++ src/test/java/dd2480/group4/net/BuildRequestTest.java
@@ -0,0 +1,28 @@
+package dd2480.group4.net;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BuildRequestTest {
+
+    @Test
+    void fromJson() throws IOException {
+
+        //GIVEN
+        String json = "{\"git_url\": \"url\", \"pusher\" : {\"name\": \"foo\", \"email\": \"bar\"}, \"after\": \"commithash\"}";
+
+        //WHEN
+        var buildRequest = BuildRequest.fromJson(json);
+
+        //THEN
+        assertEquals("foo", buildRequest.pusher.name, "name should be foo");
+        assertEquals("bar", buildRequest.pusher.email, "email should be bar");
+        assertEquals("commithash", buildRequest.hashId, "hashId should be commithash");
+        assertEquals("url", buildRequest.url, "url should be url");
+
+
+    }
+}
