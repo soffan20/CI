b79b501fbe61433585a041ab45962ddf4dd02775
Daniel Schmekel <daniel.schmekel@gmail.com>
2020-02-05 13:54:42 +0100

    Additional documentation

--- src/main/java/dd2480/group4/execute/Executor.java
+++ src/main/java/dd2480/group4/execute/Executor.java
@@ -2,7 +2,13 @@ package dd2480.group4.execute;
 
 import dd2480.group4.net.pushEvent;
 
+/**
+ * Interface for running a build.
+ */
 public interface Executor {
-
+    /**
+     * Runs the build
+     * @param req the request to be built.
+     */
     void runBuild(pushEvent req);
 }
--- src/main/java/dd2480/group4/storage/RepositoryHandler.java
+++ src/main/java/dd2480/group4/storage/RepositoryHandler.java
@@ -3,16 +3,35 @@ package dd2480.group4.storage;
 import java.io.IOException;
 import java.nio.file.Path;
 
+/**
+ * The interface for handling repositories.
+ */
 public interface RepositoryHandler {
 
+    /**
+     * Creates a temporary directory and returns the path to it.
+     * @return the path to the directory
+     * @throws IOException if it fails to create the repository
+     */
     public static Path createDirectory() throws IOException {
         return null;
     }
 
-    public static void cloneGit(Path path) {
+    /**
+     * Clones a repository to the given path.
+     * @param path the location where the repositories is cloned to.
+     * @param repo the http-address to the repo to be cloned.
+     * @throws IOException if it fails to write to the location.
+     */
+    public static void cloneGit(Path path, String repo) {
 
     }
 
+    /**
+     * Deletes the directory at the given path.
+     * @param path the path to the directory which is to be deleted.
+     * @throws IOException if if is not allowed to delete teh directory.
+     */
     public static void deleteDirectory(Path path) throws IOException {
 
     }
