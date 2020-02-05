432346a7f173873e0c7aa9af34167b6fc04a6bf6
Lizzie Queen <louiseze@kth.se>
2020-02-05 13:51:28 +0100

    feature/documentation: adding documentation #27
    Adding documentation for classes http, notification, pushevent

--- src/main/java/dd2480/group4/net/Notification.java
+++ src/main/java/dd2480/group4/net/Notification.java
@@ -11,10 +11,10 @@ import java.net.URL;
 public class Notification {
 
     /**
-     * @param pushEvent The information of the repository name, owner name and hashid where the message gets sent
+     * @param pushEvent The json classes from request from GitHub
      * @param status The status sent.
      * @param statusMessage The message accompanying the status.
-     * @return The Http-connection with the given URL where status and message is sent.
+     * @return The Http-connection with the given URL with payload and body
      * @throws IOException if it fails to send the message.
      */
     public HttpURLConnection setStatus(pushEvent pushEvent, Status status, String statusMessage) throws IOException {
@@ -25,6 +25,10 @@ public class Notification {
         String jsonBody = "{\"state\": " + status + ", \"description\" : " + statusMessage + ", \"context\" :  \"soffan20/CI\"}";
         return Http.post(statusEndpoint, jsonBody);
     }
+
+    /**
+     * Valid statuses for updating GitHub commit status
+     */
 
     enum Status {
         ERROR,
