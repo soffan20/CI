0f2b5809ab5365595c65c539f752243feec4d9a4
Daniel Schmekel <daniel.schmekel@gmail.com>
2020-02-06 13:02:39 +0100

    Feature: Added notification test fix #47

---
+++ src/test/java/dd2480/group4/net/NotificationTest.java
@@ -0,0 +1,66 @@
+package dd2480.group4.net;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NotificationTest {
+
+
+    private class Foo extends HttpURLConnection implements HttpInterface{
+        public URL url;
+        public String json;
+        public Foo(URL url, String json){
+            super(url);
+            this.url = url;
+            this.json = json;
+        };
+
+        @Override
+        public HttpURLConnection post(URL url, String json) throws IOException {
+            return new Foo(url, json);
+        }
+
+        @Override
+        public void disconnect() {
+
+        }
+
+        @Override
+        public boolean usingProxy() {
+            return false;
+        }
+
+        @Override
+        public void connect() throws IOException {
+
+        }
+    }
+
+    @Test
+    public void setStatusTest() throws IOException{
+        Notification notification = new Notification(new Foo(null, null));
+        PushEvent pe = new PushEvent();
+        pe.repository = new PushEvent.Repository();
+        pe.repository.name = "Repo-name";
+        pe.repository.url = "URL";
+        pe.repository.owner = new PushEvent.Owner();
+        pe.repository.owner.name = "Repo-owner";
+        pe.hashId = "2";
+        pe.pusher = new PushEvent.Pusher();
+        pe.pusher.name = "Pusher-name";
+        pe.pusher.email = "a@mail.com";
+
+        Foo result = (Foo)notification.setStatus(pe, Notification.Status.PENDING, "foo");
+        String json = result.json;
+        URL url = result.url;
+        assertEquals("https://api.github.com/repos/" + "Repo-owner/" + "Repo-name/" + "statuses/" + 2, url.toString(), "The URL is incorrect");
+        String[] parts = json.split(" ");
+        assertEquals("\"pending\",", parts[1], "The status message is incorrect");
+        assertEquals("\"foo\",", parts[4], "The description is incorrect");
+    }
+}
